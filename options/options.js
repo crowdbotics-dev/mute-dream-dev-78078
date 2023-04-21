b77a1a72ce0dbb52e78811c0361dac8d02a968ae
Crowdbotics <engineering@crowdbotics.com>
2023-04-21 00:18:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mute-dream-dev-78078",
+  url: "https://mute-dream-dev-78078.botics.co",
+  api: "https://mute-dream-dev-78078.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
